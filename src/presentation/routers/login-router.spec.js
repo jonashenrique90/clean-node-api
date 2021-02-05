aeb2d8159b4041dfc022d411bf127be594acb969
jonashenrique90 <jonas.herique90@gmail.com>
2021-02-05 19:26:06 -0300

    ensure LoginRouter returns 400 if no password is provider

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -1,8 +1,9 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
-        statusCode: 400
+        statusCode: 400,
+        message: 'Email/password'
       }
     }
   }
@@ -17,5 +18,17 @@ describe('LoginRouter', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toHaveProperty('message')
+  })
+  it('Should return 400 if no password is provider', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse).toHaveProperty('message')
   })
 })
