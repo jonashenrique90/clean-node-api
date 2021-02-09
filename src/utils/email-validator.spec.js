72b33ce0708ab9add013e92faa3dd277bf82d5e8
jonashenrique90 <jonas.herique90@gmail.com>
2021-02-09 10:24:34 -0300

    add tests for emailValidator; refactor errors

---
+++ src/utils/email-validator.spec.js
@@ -0,0 +1,20 @@
+const validator = require('validator')
+class EmailValidator {
+  isValid (email) {
+    return validator.isEmail(email)
+  }
+}
+
+describe('Email Validator', () => {
+  it('should return true if validator returns true', () => {
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('valid_email@gmail.com')
+    expect(isEmailValid).toBe(true)
+  })
+  it('should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = new EmailValidator()
+    const isEmailValid = sut.isValid('invalid_email')
+    expect(isEmailValid).toBe(false)
+  })
+})
