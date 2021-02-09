5b473e3990aff8f82201cd31769ee0c91b0e1d9f
jonashenrique90 <jonas.herique90@gmail.com>
2021-02-09 10:33:42 -0300

    move EmailValidator to its own file; create mock for validator

--- src/utils/email-validator.spec.js
+++ src/utils/email-validator.spec.js
@@ -1,20 +1,25 @@
 const validator = require('validator')
-class EmailValidator {
-  isValid (email) {
-    return validator.isEmail(email)
-  }
+const EmailValidator = require('./email-validator')
+
+const makeSut = () => {
+  return new EmailValidator()
 }
 
 describe('Email Validator', () => {
   it('should return true if validator returns true', () => {
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('valid_email@gmail.com')
     expect(isEmailValid).toBe(true)
   })
   it('should return false if validator returns false', () => {
     validator.isEmailValid = false
-    const sut = new EmailValidator()
+    const sut = makeSut()
     const isEmailValid = sut.isValid('invalid_email')
     expect(isEmailValid).toBe(false)
+  })
+  it('should call validator with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('any_email@gmail.com')
+    expect(validator.email).toBe('any_email@gmail.com')
   })
 })
