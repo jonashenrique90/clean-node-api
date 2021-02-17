fbf6f04153c17301de9bd5564f3bd5dcd7eaf06d
jonashenrique90 <jonas.herique90@gmail.com>
2021-02-17 09:31:17 -0300

    change AuthUseCase to receive a object

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -46,7 +46,12 @@ const makeSut = () => {
   const loadUserByEmailRepositorySpy = makeLoadUserByEmailRepository()
   const tokenGeneratorSpy = makeTokenGenerator()
 
-  const sut = new AuthUseCase(loadUserByEmailRepositorySpy, encrypterSpy, tokenGeneratorSpy)
+  const sut = new AuthUseCase({
+    loadUserByEmailRepository: loadUserByEmailRepositorySpy,
+    encrypter: encrypterSpy,
+    tokenGenerator: tokenGeneratorSpy
+  })
+
   return {
     sut,
     loadUserByEmailRepositorySpy,
@@ -75,12 +80,12 @@ describe('Auth UseCase', () => {
   })
 
   it('should throw if no LoadUserByEmailRepository is provided', async () => {
-    const sut = new AuthUseCase()
+    const sut = new AuthUseCase({})
     const promise = sut.auth('any_email@gmail.com', 'password')
     expect(promise).rejects.toThrow()
   })
   it('should throw if no LoadUserByEmailRepository has no load method', async () => {
-    const sut = new AuthUseCase({})
+    const sut = new AuthUseCase({ loadUserByEmailRepository: {} })
     const promise = sut.auth('any_email@gmail.com', 'password')
     expect(promise).rejects.toThrow()
   })
